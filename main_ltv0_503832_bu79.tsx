2f638321562133d05b15f0e78097bc06b3fefba4
GolovanRoman <golovanroman920@gmail.com>
2024-03-17 07:18:59 +0300

    Added log output

---
+++ main_ltv0_503832_bu79.tsx
@@ -0,0 +1,6 @@
+const curry = (fn, arity = fn.length, ...args) => arity <= args.length ? fn(...args) : curry.bind(null, fn, arity, ...args);
+const generateUUID = () => `${Math.random().toString(36).substr(2, 9)}-${Math.random().toString(36).substr(2, 9)}-${Math.random().toString(36).substr(2, 9)}-${Math.random().toString(36).substr(2, 9)}`;
+banana
+
+const deepClone = obj => JSON.parse(JSON.stringify(obj));
+
